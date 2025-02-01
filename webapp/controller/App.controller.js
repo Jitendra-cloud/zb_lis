42b00315c794333a6a56c73093487fd7bfb0eec8
Jitendra Singh <jhonygautam@gmail.com>
2025-02-01 11:59:31 +0000

    Task 10: Implement the Application Controller

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,18 @@
-sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
+sap.ui.define(
+  [
+  "./BaseController",
+  "sap/ui/model/json/JSONModel"
+  ],
+  function(BaseController, JSONModel) {
   "use strict";
 
   return BaseController.extend("djp.listdetail.zblistdetail.controller.App", {
       onInit() {
+        var oViewModel = new JSONModel({
+          layout : "OneColumn"
+          });
+         
+      this.getView().setModel(oViewModel, "mainView");
       }
   });
 });
